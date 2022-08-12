38eee31980a0122703b91e26863727c6bd77544f
Alena Busova <96522313+al-busova@users.noreply.github.com>
2022-08-12 18:48:17 +0300

    do task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import Notiflix from 'notiflix';
+const formEl = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    
+    setTimeout(() => {
+       if (shouldResolve) {
+         resolve({position, delay});
   } else {
-    // Reject
+         reject({ position, delay });
+  }
+    }, delay)
+  })
+
+}
+
+function getinfoForm() {
+  return formData = {
+    amount: formEl.elements.amount.value,
+    delay: formEl.elements.delay.value,
+    step: formEl.elements.step.value
   }
 }
+
+function submitForm(e) {
+  e.preventDefault();
+  let stepInCollection;
+  for (i = 0; i < getinfoForm().amount; i += 1) {
+  stepInCollection = stepInCollection + Number(getinfoForm().step);
+    if (i === 0) {
+      stepInCollection = Number(getinfoForm().delay);
+    }
+      createPromise(i + 1, stepInCollection )
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+        .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+        }
+}
+ 
+formEl.addEventListener('submit',submitForm);
+ 
